c9f78ae3f0764df1498aa48aa074d47552402296
Tani2409 <156881741+Tani2409@users.noreply.github.com>
2025-10-07 10:09:01 +0700

    Update ex02.py

--- Ass/ex02.py
+++ Ass/ex02.py
@@ -1,20 +1,3 @@
-budget = input('what is your total budget: $')
-avaiable_items = {'Apple' : 10, 'Milk': 20, 'Chips': 30, 'Carrot': 40}
-cart =  []
-total_budget = 0
-discounts = {'Fruit': 10, 'Vegatable': 15, 'Dairy': 20, 'Snacks': 25}
-category = ['Fruit', 'Vegatable', 'Dairy', 'Snacks']
-
-print (f'Available items:  {avaiable_items}')
-item = input('enter item to add to cart: ').capitalize()
-if item in avaiable_items:
-    cart.append(item)
-    discount_code = input(f'enter category {category}:').capitalize()
-    if discount_code in discounts:
-        discount = discounts.get(discount_code, 0)
-        print (f'Applied {discount}% discount!')
-        print (f'Added {item}. Remaining budget: ${int(budget) - avaiable_items[item] + (avaiable_items[item] * discount / 100)}')
-
 # Exercise 2: Advanced Grocery Shopping Tracker
 # Based on an exercise 1, write a program to enhance the budget tracker to include item names,
 # categorize purchases, apply discounts, and generate a detailed receipt.
